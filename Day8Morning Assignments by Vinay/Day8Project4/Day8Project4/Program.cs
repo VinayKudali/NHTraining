4ac3ff578bdb650d0b6dc8a66c356bf560ad09b7
VinayKudali <vinaykudali@gmail.com>
2022-02-03 16:33:44 +0530

    Day8 Submission

---
+++ Day8Morning Assignments by Vinay/Day8Project4/Day8Project4/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day8Project4
+{
+    class Department
+    {
+        public int Id;
+        public string name;
+        public int empcount;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /****************************************************************
+             Author: Vinay Kudali
+            Purpose: Creating a department class and adding variables and printing dept id, dept name
+            ******************************************************************/
+            List<Department> dept = new List<Department>()
+            {
+                new Department() { Id = 101, name ="HR Department", empcount = 5},
+
+                new Department() { Id = 201, name ="Quality Analyst", empcount = 50},
+                new Department() { Id = 301, name ="Adminstration",empcount = 30},
+                new Department() { Id = 401, name ="Production", empcount = 34},
+                new Department() { Id = 501, name ="Finance",  empcount = 13}
+            };
+
+
+
+            //Using For Loop
+            for (int i = 0; i < dept.Count; i++)
+            {
+                if (dept[i].empcount > 25)
+                    Console.WriteLine($"name={dept[i].name}, Id={dept[i].Id}");
+            }
+
+            //Using foreach loop
+            foreach (var d in dept)
+            {
+                if (d.empcount > 25)
+                    Console.WriteLine($"name={d.name}, Id={d.Id}");
+            }
+            //using Lambda Expression
+            dept.Where(x => x.empcount > 25).ToList().ForEach(x => Console.WriteLine($"name={x.name}, Id={x.Id}"));
+
+
+            //Even numbers using LINQ Query
+            var result = from v in dept
+                         where v.empcount > 25
+                         select v;
+            result.ToList().ForEach(x => Console.WriteLine($"name={x.name}, Id={x.Id}"));
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Day8Morning Assignments by Vinay/Day8Project5/Day8Project5/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day8Project5
+{
+
+
+    //Author: Vinay Kudali
+    //Program: Own class with own variables and calling for,foreach,Lamda,LINQ
+    class Institute
+    {
+        public string name;
+        public string location;
+        public string type;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Institute> off = new List<Institute>()
+                {
+                    new Institute() { name = "Jspiders", location = "Kphb", type = "Full stack java" },
+                    new Institute() { name = "Qspiders", location = "Panjagutta", type = " Testing " },
+                    new Institute() { name = "Manya", location = "Bangalore", type = "Python" },
+                    new Institute() { name = "Texas", location = "kukatpally", type = "IELTS" },
+                    new Institute() { name = "CV corp", location = "raidurg", type = "java" }
+                };
+            //for loop
+            for (int i = 0; i < off.Count; i++)
+            {
+                Console.WriteLine($"name={off[i].name}, location={off[i].location}, type={off[i].type}");
+            }
+
+            //foreach loop
+            foreach (var o in off)
+            {
+                Console.WriteLine($"name={o.name}, location={o.location}, type={o.type}");
+            }
+
+            //Lamda Expression
+
+            off.ToList().ForEach(o => Console.WriteLine($"name={o.name}, location={o.location}, type={o.type}"));
+
+            //LINQ Query
+            var result = from o in off
+                         select o;
+            result.ToList().ForEach(o => Console.WriteLine($"name={o.name}, location={o.location}, type={o.type}"));
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
