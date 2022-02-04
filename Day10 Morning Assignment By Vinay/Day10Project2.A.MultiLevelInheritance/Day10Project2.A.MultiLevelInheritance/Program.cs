c3f1bc5d12bd2f54973eb4607ba20127801f19b9
VinayKudali <vinaykudali@gmail.com>
2022-02-04 19:03:10 +0530

    day 10 submission

---
+++ Day10 Morning Assignment By Vinay/Day10Project2.A.MultiLevelInheritance/Day10Project2.A.MultiLevelInheritance/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+
+
+namespace Day10Project1
+
+{
+
+    //Author: Vinay Kudali 
+
+    //Purpose: Single Level inheritance 
+
+    class AddSub
+    {
+
+        /// <summary> 
+        ///This method will perform addition 
+        /// </summary> 
+        /// <param name="p"></param> 
+        /// <param name="q"></param> 
+        /// <returns>Add</returns> 
+
+        public int Add(int p, int q)
+
+        {
+            return p + q;
+        }
+
+        /// <summary> 
+        /// This method will perform Substraction 
+        /// </summary> 
+        /// <param name="p"></param> 
+        /// <param name="q"></param> 
+        /// <returns>Substract</returns> 
+
+        public int Substract(int p, int q)
+
+        {
+            return p - q;
+        }
+
+    }
+
+    class Multiply : AddSub
+
+    {
+        /// <summary> 
+        /// This method will perform Multiplication  
+        /// </summary> 
+        /// <param name="p"></param> 
+        /// <param name="q"></param> 
+        /// <returns>mul</returns> 
+
+        public int Mul(int p, int q)
+
+        {
+            return p * q;
+        }
+
+    }
+
+  
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            Multiply n1 = new Multiply();
+            Console.WriteLine(n1.Add(12, 8));
+            Console.WriteLine(n1.Substract(9, 7));
+            Console.WriteLine(n1.Mul(6, 3));
+
+            Console.ReadLine();
+
+        }
+
+    }
+
+}
---
+++ Day10 Morning Assignment By Vinay/day10Project1/day10Project1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day10Project1
+{
+    //Author: Vinay Kudali
+    //Purpose: Multi Level inheritance
+    class AddSub
+
+    { 
+        /// <summary>
+        ///This method will perform addition
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="q"></param>
+        /// <returns>Add</returns>
+        public int Add(int p, int q)
+        {
+            return p + q;
+        }
+        /// <summary>
+        /// This method will perform Substraction
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="q"></param>
+        /// <returns>Substract</returns>
+        public int Substract(int p, int q)
+        {
+            return p - q;
+        }
+
+    }
+    class Multiply : AddSub
+    {
+        /// <summary>
+        /// This method will perform Multiplication 
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="q"></param>
+        /// <returns>mul</returns>
+        public int Mul(int p, int q)
+        {
+            return p * q;
+        }
+    }
+    class Division : Multiply
+    {
+        /// <summary>
+        /// this method will perform Division
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="q"></param>
+        /// <returns>Div</returns>
+        public int Div(int p, int q)
+        {
+            return p / q;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Division n1 = new Division();
+            Console.WriteLine(n1.Add(12, 8));
+            Console.WriteLine(n1.Substract(9, 7));
+            Console.WriteLine(n1.Mul(6, 3));
+            Console.WriteLine(n1.Div(9, 3));
+            Console.ReadLine();
+        }
+    }
+}
