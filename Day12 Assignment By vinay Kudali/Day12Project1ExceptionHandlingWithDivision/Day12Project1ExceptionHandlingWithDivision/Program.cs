c6fc80e1f35c0f262f13bc79356fa4aa6c3fa496
VinayKudali <vinaykudali@gmail.com>
2022-02-08 18:16:46 +0530

    Day 12 Assignment Submission

---
+++ Day12 Assignment By vinay Kudali/Day12Project1ExceptionHandlingWithDivision/Day12Project1ExceptionHandlingWithDivision/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day12Project1ExceptionHandlingWithDivision
+{
+    internal class Program
+    {
+        //Author Vinay Kudali
+        //Purpose: Creating 3 Type of exceptions with division
+        static void Main(string[] args)
+        {
+
+
+            string s = "this";
+            s = s.Insert(2, "is");
+                
+                
+            
+            
+             
+        }
+    }
+}
---
+++ Day12 Assignment By vinay Kudali/Day12projectCompailation Error 1/Day12projectCompailation Error 1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day12projectCompailation_Error_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] data = new char[7];
+            data[11] = '#';
+           
+        }
+    }
+}
